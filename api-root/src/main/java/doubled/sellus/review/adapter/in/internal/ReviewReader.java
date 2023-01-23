f6626154df2c585d734ec7eccb70ce3839591252
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-24 04:21:55 +0900

    fix: 리뷰 reviewReader 잘못 된 responseEntity 제거

--- api-root/src/main/java/doubled/sellus/review/adapter/in/internal/ReviewReader.java
+++ api-root/src/main/java/doubled/sellus/review/adapter/in/internal/ReviewReader.java
@@ -1,16 +1,15 @@
 package doubled.sellus.review.adapter.in.internal;
 
 import doubled.sellus.review.Review;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface ReviewReader {
 
-    ResponseEntity<Review> getReview(Long id);
+    Review getReview(Long id);
 
-    ResponseEntity<List<Review>> getReviewByUserId(Long userId);
+    List<Review> getReviewByUserId(Long userId);
 
-    ResponseEntity<List<Review>> getReviewByProductId(Long productId);
+    List<Review> getReviewByProductId(Long productId);
 
 }
