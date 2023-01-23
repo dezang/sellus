0a5b798490d51b8544ea304d6796099884ea1419
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-24 04:10:43 +0900

    fix: 상품 생성시 productCreateRequest 로 받도록 수정

--- api-root/src/main/java/doubled/sellus/product/adapter/in/internal/ProductCreator.kt
+++ api-root/src/main/java/doubled/sellus/product/adapter/in/internal/ProductCreator.kt
@@ -1,10 +1,10 @@
 package doubled.sellus.product.adapter.`in`.internal
 
-import doubled.sellus.product.domain.CreateProductDomain
+import doubled.sellus.product.adapter.`in`.rest.request.ProductCreateRequest
 import doubled.sellus.product.domain.Product
 
 interface ProductCreator {
 
-    fun createProduct(product: CreateProductDomain): Product?
+    fun createProduct(product: ProductCreateRequest): Product?
 
 }
