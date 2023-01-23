4ccad5ae64fad4f0fbd61a4b33fce93320c71c37
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-24 00:27:27 +0900

    refactor: JpaˆEntity builder 추가

--- api-root/src/main/kotlin/doubled/sellus/product/adapter/out/ProductJpaEntity.java
+++ api-root/src/main/kotlin/doubled/sellus/product/adapter/out/ProductJpaEntity.java
@@ -2,6 +2,7 @@ package doubled.sellus.product.adapter.out;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 class ProductJpaEntity {
 
     @Id
