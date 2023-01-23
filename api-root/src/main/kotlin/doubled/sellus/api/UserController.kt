9fc638d46f00f0220fbe0bce33744845211f5e49
Dezang <dezang@dezang.net>
2023-01-23 23:36:52 +0900

    feat: 사용자 프로필 정보 업데이트 API 추가

--- api-root/src/main/kotlin/doubled/sellus/api/UserController.kt
+++ api-root/src/main/kotlin/doubled/sellus/api/UserController.kt
@@ -1,11 +1,14 @@
 package doubled.sellus.api
 
+import doubled.sellus.user.UserProfileService
+import doubled.sellus.user.UserProfileUpdate
 import doubled.sellus.user.UserReader
 import doubled.sellus.user.UserSignUpService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("users")
 class UserController(
-    val userSignUpService: UserSignUpService,
     val userReader: UserReader,
+    val userSignUpService: UserSignUpService,
+    val userProfileService: UserProfileService,
 ) {
     @PostMapping
     fun add(@RequestBody request: UserRequest): ResponseEntity<Void> {
@@ -26,5 +30,11 @@ class UserController(
     fun get(@PathVariable userId: Long): ResponseEntity<UserView> {
         val foundUser = userReader.getById(userId)
         return ResponseEntity.ok().body(UserView.of(foundUser))
+    }
+
+    @PutMapping("{userId}")
+    fun update(@PathVariable userId: Long, @RequestBody request: UserProfileUpdate): ResponseEntity<Void> {
+        userProfileService.update(userId, request)
+        return ResponseEntity.ok().build()
     }
 }
