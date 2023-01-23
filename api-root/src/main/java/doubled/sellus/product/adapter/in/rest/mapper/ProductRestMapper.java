0919cc8e3e4968424ee215e1667d322e389b4cbb
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-24 02:57:26 +0900

    fix: ProductRestMapper 에 component 어노테이션 추가

--- api-root/src/main/java/doubled/sellus/product/adapter/in/rest/mapper/ProductRestMapper.java
+++ api-root/src/main/java/doubled/sellus/product/adapter/in/rest/mapper/ProductRestMapper.java
@@ -1,9 +1,11 @@
- package doubled.sellus.product.adapter.in.rest.mapper;
+package doubled.sellus.product.adapter.in.rest.mapper;
 
 import doubled.sellus.product.adapter.in.rest.request.ProductCreateRequest;
 import doubled.sellus.product.domain.Product;
 import doubled.sellus.product.domain.ProductDomain;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductRestMapper {
 
     public Product toProduct(ProductCreateRequest productCreateRequest) {
