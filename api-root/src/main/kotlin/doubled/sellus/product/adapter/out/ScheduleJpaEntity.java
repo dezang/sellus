7b5e283116e7c94e45c28d3812859fb7bf68c6be
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-24 00:26:51 +0900

    refactor: 스케쥴 접근권한자 package-private 으로 변경

--- api-root/src/main/kotlin/doubled/sellus/product/adapter/out/ScheduleJpaEntity.java
+++ api-root/src/main/kotlin/doubled/sellus/product/adapter/out/ScheduleJpaEntity.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ScheduleJpaEntity {
+class ScheduleJpaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
