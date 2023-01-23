1a2f94b0b7e7c629a07a7f7673743b2abda9617e
dongho.geum <dongho.geum@myrealtrip.com>
2023-01-24 03:37:51 +0900

    feat: review Reader interface 추가

---
+++ api-root/src/main/java/doubled/sellus/review/adapter/in/rest/ReviewReader.java
@@ -0,0 +1,16 @@
+package doubled.sellus.review.adapter.in.rest;
+
+import doubled.sellus.review.Review;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface ReviewReader {
+
+    ResponseEntity<Review> getReview(Long id);
+
+    ResponseEntity<List<Review>> getReviewByUserId(Long userId);
+
+    ResponseEntity<List<Review>> getReviewByProductId(Long productId);
+
+}
